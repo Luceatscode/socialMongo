00891b62cc2348dbdefa8679669f66a35c554949
Luceatscode <lucsw92@hotmail.com>
2023-07-17 11:47:19 -0400

    set up routes

--- server.js
+++ server.js
@@ -1,6 +1,75 @@
 const express = require('express');
 const app = express();
 
+app.get('/api/users', (req, res) => {
+    res.send('Hello World!')
+  });
+
+  app.get('/api/users/:id', (req, res) => {
+    res.send('Hello World!')
+  });
+
+app.post('/api/users', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.put('/api/users/:id', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.delete('/api/users/:id', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.post('/api/users/:userId/friends/:friendId', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.delete('/api/users/:userId/friends/:friendId', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/api/thoughts', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.get('/api/thoughts/:id', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.post('/api/thoughts', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.put('/api/thoughts', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.delete('/api/thoughts', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.post('/api/thoughts/:thoughtId/reactions', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.delete('/api/thoughts/:thoughtId/:reactionId', (req, res) => {
+    res.send('Hello World!')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(3000, () => {
     console.log('server started');
 });
+
