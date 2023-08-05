73c004d34c15578ad89c9b254d9a4e93b8877dd5
Luceatscode <lucsw92@hotmail.com>
2023-08-05 11:25:47 -0600

    created models

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const { Schema, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required',
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: 'Email is required',
+            unique: true,
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
