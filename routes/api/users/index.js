a0b211acafd05883676a32a4baac2ad28379b980
Luceatscode <lucsw92@hotmail.com>
2023-08-05 10:08:31 -0600

    created thought routes

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -2,31 +2,31 @@ const router = require('express').Router();
 
 
 router.get('/', (req, res) => {
-    res.send('get/api/users')
+    res.json('get/api/users')
 });
 
 router.get('/:id', (req, res) => {
-    res.send('get/api/users/:id')
+    res.json('get/api/users/:id')
 });
 
 router.post('/', (req, res) => {
-    res.send('post/api/users')
+    res.json('post/api/users')
 });
 
 router.put('/:id', (req, res) => {
-    res.send('put/api/users/:id')
+    res.json('put/api/users/:id')
 });
 
 router.delete('/:id', (req, res) => {
-    res.send('delete/api/users/:id')
+    res.json('delete/api/users/:id')
 });
 
 router.post('/:userId/friends/:friendId', (req, res) => {
-    res.send('post/api/users/:userId/friends/:friendId')
+    res.json('post/api/users/:userId/friends/:friendId')
 });
 
 router.delete('/:userId/friends/:friendId', (req, res) => {
-    res.send('delete/api/users/:userId/friends/:friendId')
+    res.json('delete/api/users/:userId/friends/:friendId')
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,33 +5,7 @@ const routes = require('./routes');
 const PORT = process.env.PORT || 3001;
 
 
-app.get('/api/thoughts', (req, res) => {
-    res.send('get/api/thoughts')
-});
 
-app.get('/api/thoughts/:id', (req, res) => {
-    res.send('get/api/thoughts/:id')
-});
-
-app.post('/api/thoughts', (req, res) => {
-    res.send('post/api/thoughts')
-});
-
-app.put('/api/thoughts', (req, res) => {
-    res.send('put/api/thoughts')
-});
-
-app.delete('/api/thoughts', (req, res) => {
-    res.send('delete/api/thoughts')
-});
-
-app.post('/api/thoughts/:thoughtId/reactions', (req, res) => {
-    res.send('post/api/thoughts/:thoughtId/reactions')
-});
-
-app.delete('/api/thoughts/:thoughtId/:reactionId', (req, res) => {
-    res.send('delete/api/thoughts/:thoughtId/:reactionId')
-});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
