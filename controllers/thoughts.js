dcfcce77c4eb4812386be2d11b24089d104b2276
Luceatscode <lucsw92@hotmail.com>
2023-08-05 12:30:01 -0600

    added thought controller

--- controllers/thoughts.js
+++ controllers/thoughts.js
@@ -1,3 +1,76 @@
-const { User, Thought } = require('../models'); 
+const { User, Thought } = require('../models');
 
-module.exports = {};
+module.exports = {
+    // get all thoughts
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find({}).populate('reactions');
+            res.json(dbThoughtData);
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
+
+
+    // get one thought by id
+    async thought({ params }, res) {
+        try {
+            const thought = await Thought.findOne({ _id: params.id }).populate('reactions')
+                .select('-__v');
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // createThought
+    async createThought({ body }, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(dbThoughtData);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    // update thought by id
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.ThoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // delete thought
+    async deleteThought({ params }, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: params.id });
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought found with this id!' });
+            }
+
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+};
